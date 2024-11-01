e8cdc5140186996bd756f169db4655c586017801
BeccaHamzy <oyewoleolufunmi@gmail.com>
2024-11-01 04:12:36 -0700

    new changes

--- src/Component/Personal/Personal.jsx
+++ src/Component/Personal/Personal.jsx
@@ -1,14 +1,14 @@
-import styled from "styled-components"
-import Hero from "../components/home/Hero"
-import Info from "../components/home/Info"
+// import styled from "styled-components"
+// import Hero from "../components/home/Hero"
+// import Info from "../components/home/Info"
 
-const Personal = () => {
-    return(
-       <div>
-     <Hero />
-     <Info />
-       </div>
-    )
-}
+// const Personal = () => {
+//     return(
+//        <div>
+//      <Hero />
+//      <Info />
+//        </div>
+//     )
+// }
 
-export default Personal
+// export default Personal
